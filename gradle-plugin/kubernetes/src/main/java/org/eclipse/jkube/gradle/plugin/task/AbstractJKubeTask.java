45e929c4fa2e1225e6b09baafc5c738b6cc942e2
Rohan Kumar <rohaan@redhat.com>
2021-07-22 15:29:24 +0530

    KubernetesBuildTask: Basic implementation for Simple Dockerfile Mode

--- gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/task/AbstractJKubeTask.java
+++ gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/task/AbstractJKubeTask.java
@@ -20,29 +20,49 @@ import org.eclipse.jkube.gradle.plugin.KubernetesExtension;
 import org.eclipse.jkube.kit.common.JKubeConfiguration;
 import org.eclipse.jkube.kit.common.JavaProject;
 import org.eclipse.jkube.kit.common.KitLogger;
+import org.eclipse.jkube.kit.common.RegistryConfig;
 import org.eclipse.jkube.kit.common.util.Slf4jKitLogger;
 import org.eclipse.jkube.kit.config.access.ClusterAccess;
 import org.eclipse.jkube.kit.config.access.ClusterConfiguration;
+import org.eclipse.jkube.kit.config.image.build.RegistryAuthConfiguration;
 import org.eclipse.jkube.kit.config.service.JKubeServiceHub;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.TaskAction;
 
 import static org.eclipse.jkube.gradle.plugin.KubernetesExtension.DEFAULT_OFFLINE;
 
 public abstract class AbstractJKubeTask extends DefaultTask implements JKubeTask {
 
+  @Internal
   private final KubernetesExtension kubernetesExtension;
+  @Input
   protected JavaProject javaProject;
+  @Input
   protected KitLogger kitLogger;
+  @Input
   protected ClusterAccess clusterAccess;
+  @Input
   protected JKubeServiceHub jKubeServiceHub;
+  @Input
+  protected RegistryAuthConfiguration authConfig;
+  @Input
+  protected boolean skipExtendedAuth;
+  @Input
+  protected String registry;
+  @Input
+  protected String pullRegistry;
+  @Internal
+  public static final String DOCKER_BUILD_TIMESTAMP = "docker/build.timestamp";
 
   protected AbstractJKubeTask(Class<? extends KubernetesExtension> extensionClass) {
     kubernetesExtension = getProject().getExtensions().getByType(extensionClass);
   }
 
   @TaskAction
+  @Input
   public final void runTask() {
     javaProject = GradleUtil.convertGradleProject(getProject());
     kitLogger = new Slf4jKitLogger(getLogger());
@@ -51,24 +71,40 @@ public abstract class AbstractJKubeTask extends DefaultTask implements JKubeTask
     run();
   }
 
+  @Input
   protected JKubeServiceHub.JKubeServiceHubBuilder initJKubeServiceHubBuilder() {
     return JKubeServiceHub.builder()
         .log(kitLogger)
         .configuration(JKubeConfiguration.builder()
             .project(javaProject)
             .reactorProjects(Collections.singletonList(javaProject))
+                .sourceDirectory("src/main/jkube")
+                .outputDirectory("build/docker")
+                .registryConfig(getRegistryConfig(pullRegistry))
             .build())
         .clusterAccess(clusterAccess)
         .offline(kubernetesExtension.getOffline().getOrElse(DEFAULT_OFFLINE))
         .platformMode(kubernetesExtension.getRuntimeMode());
   }
 
+  @Input
   protected ClusterConfiguration initClusterConfiguration() {
     return ClusterConfiguration.from(kubernetesExtension.access,
         System.getProperties(), javaProject.getProperties()).build();
   }
 
+  @Input
+  @Internal
   protected final JKubeServiceHub getJKubeServiceHub() {
     return jKubeServiceHub;
+  }
+
+  protected RegistryConfig getRegistryConfig(String specificRegistry) {
+    return RegistryConfig.builder()
+            .settings(Collections.emptyList())
+            .authConfig(authConfig != null ? authConfig.toMap() : null)
+            .skipExtendedAuth(skipExtendedAuth)
+            .registry(specificRegistry != null ? specificRegistry : registry)
+            .build();
   }
 }
--- gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/task/KubernetesBuildTask.java
+++ gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/task/KubernetesBuildTask.java
@@ -16,6 +16,30 @@ package org.eclipse.jkube.gradle.plugin.task;
 import javax.inject.Inject;
 
 import org.eclipse.jkube.gradle.plugin.KubernetesExtension;
+import org.eclipse.jkube.kit.build.api.helper.DockerFileUtil;
+import org.eclipse.jkube.kit.build.service.docker.DockerAccessFactory;
+import org.eclipse.jkube.kit.build.service.docker.ImagePullManager;
+import org.eclipse.jkube.kit.build.service.docker.ServiceHubFactory;
+import org.eclipse.jkube.kit.build.service.docker.access.DockerAccess;
+import org.eclipse.jkube.kit.build.service.docker.access.log.LogOutputSpecFactory;
+import org.eclipse.jkube.kit.build.service.docker.helper.ConfigHelper;
+import org.eclipse.jkube.kit.build.service.docker.helper.ImageNameFormatter;
+import org.eclipse.jkube.kit.common.util.EnvUtil;
+import org.eclipse.jkube.kit.config.image.ImageConfiguration;
+import org.eclipse.jkube.kit.config.image.build.JKubeBuildStrategy;
+import org.eclipse.jkube.kit.config.resource.BuildRecreateMode;
+import org.eclipse.jkube.kit.config.service.BuildServiceConfig;
+import org.eclipse.jkube.kit.config.service.JKubeServiceException;
+import org.eclipse.jkube.kit.config.service.JKubeServiceHub;
+import org.gradle.api.tasks.Internal;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Properties;
+
+import static org.eclipse.jkube.kit.common.util.PropertiesUtil.getValueFromProperties;
 
 @SuppressWarnings("CdiInjectionPointsInspection")
 public class KubernetesBuildTask extends AbstractJKubeTask {
@@ -27,7 +51,92 @@ public class KubernetesBuildTask extends AbstractJKubeTask {
   }
 
   @Override
+  protected JKubeServiceHub.JKubeServiceHubBuilder initJKubeServiceHubBuilder() {
+    JKubeServiceHub.JKubeServiceHubBuilder builder = super.initJKubeServiceHubBuilder();
+    DockerAccessFactory.DockerAccessContext dockerAccessContext = getDockerAccessContext();
+    DockerAccessFactory dockerAccessFactory = new DockerAccessFactory();
+    DockerAccess access = dockerAccessFactory.createDockerAccess(dockerAccessContext);
+    LogOutputSpecFactory logSpecFactory = new LogOutputSpecFactory(true, true, null);
+    ServiceHubFactory serviceHubFactory = new ServiceHubFactory();
+    builder.dockerServiceHub(serviceHubFactory.createServiceHub(access, kitLogger, logSpecFactory));
+    builder.buildServiceConfig(buildServiceConfigBuilder().build());
+
+    return builder;
+  }
+
+  @Override
   public void run() {
-    throw new UnsupportedOperationException("To be implemented");
+    try {
+      if (DockerFileUtil.isSimpleDockerFileMode(javaProject.getBaseDirectory())) {
+        kitLogger.info("Simple DockerFile mode detected");
+        File dockerFile = new File(javaProject.getBaseDirectory(), "Dockerfile");
+        ImageNameFormatter imageNameFormatter = new ImageNameFormatter(javaProject, getBuildTimestamp());
+        String defaultImageName = imageNameFormatter.format(getValueFromProperties(javaProject.getProperties(),
+                "jkube.image.name", "jkube.generator.name"));
+        ImageConfiguration imageConfiguration = DockerFileUtil.createSimpleDockerfileConfig(dockerFile, defaultImageName);
+        ConfigHelper.initAndValidate(Collections.singletonList(imageConfiguration), null, imageNameFormatter);
+        jKubeServiceHub.getBuildService().build(imageConfiguration);
+      }
+    } catch (JKubeServiceException | IOException e) {
+      kitLogger.error(e.getMessage());
+    }
+  }
+
+  @Internal
+  protected DockerAccessFactory.DockerAccessContext getDockerAccessContext() {
+    return DockerAccessFactory.DockerAccessContext.builder()
+            .projectProperties(javaProject.getProperties())
+            .log(kitLogger)
+            .maxConnections(10)
+            .build();
+  }
+
+  protected BuildServiceConfig.BuildServiceConfigBuilder buildServiceConfigBuilder() {
+    return BuildServiceConfig.builder()
+            .buildRecreateMode(BuildRecreateMode.fromParameter("none"))
+            .jKubeBuildStrategy(JKubeBuildStrategy.docker)
+            .forcePull(true)
+            .imagePullManager(getImagePullManager(null, null))
+            .buildDirectory(javaProject.getBuildDirectory().getPath());
+  }
+
+  public ImagePullManager getImagePullManager(String imagePullPolicy, String autoPull) {
+    return new ImagePullManager(getSessionCacheStore(), imagePullPolicy, autoPull);
+  }
+
+  @Internal
+  protected synchronized Date getBuildTimestamp() throws IOException {
+    return getReferenceDate();
+  }
+
+  // Get the reference date for the build. By default this is picked up
+  // from an existing build date file. If this does not exist, the current date is used.
+  @Internal
+  protected Date getReferenceDate() throws IOException {
+    Date referenceDate = EnvUtil.loadTimestamp(getBuildTimestampFile());
+    return referenceDate != null ? referenceDate : new Date();
+  }
+
+  // used for storing a timestamp
+  @Internal
+  protected File getBuildTimestampFile() {
+    return new File(javaProject.getBuildDirectory(), DOCKER_BUILD_TIMESTAMP);
+  }
+
+  @Internal
+  protected ImagePullManager.CacheStore getSessionCacheStore() {
+    return new ImagePullManager.CacheStore() {
+      @Override
+      public String get(String key) {
+        Properties userProperties = javaProject.getProperties();
+        return userProperties.getProperty(key);
+      }
+
+      @Override
+      public void put(String key, String value) {
+        Properties userProperties = javaProject.getProperties();
+        userProperties.setProperty(key, value);
+      }
+    };
   }
 }
